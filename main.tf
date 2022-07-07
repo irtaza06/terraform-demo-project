35c7b44972c85d1724ac2331d7ac43daa8451bc4
irtaza06 <irtazahassan06@gmail.com>
2022-07-07 12:53:22 +0200

    Added SG and ingress, egress rules

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip_addr" {}
 
 provider "aws" {
   region = "eu-central-1"
@@ -56,7 +57,35 @@ resource "aws_internet_gateway" "terra-igw" {
 
 
 
-/*resource "aws_route_table_association" "as-rtb-subnet1" {
+resource "aws_route_table_association" "as-rtb-subnet1" {
   subnet_id = aws_subnet.terra-subnet-1.id
-  route_table_id = aws_route_table.terra-rtb.id
-}*/
+  route_table_id = aws_vpc.terra-vpc.default_route_table_id 
+}
+
+
+resource "aws_security_group" "terra-sg" {
+  name =  "terra-sg"
+  vpc_id = aws_vpc.terra-vpc.id
+  ingress {
+    from_port = 22 
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip_addr]
+  }
+   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+        Name = "${var.env_prefix}-sg"
+          }
+}
